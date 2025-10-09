d3ad0bb9d43462798c07604c55546eb7049054c7
sujitphalake-13 <sujit.phalake13@gmail.com>
2025-10-09 22:54:20 +0530

    Static Table in selenium

--- src/test/java/day31/Practice.java
+++ src/test/java/day31/Practice.java
@@ -2,49 +2,42 @@ package day31;
 
 import java.time.Duration;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AutoSuggestionDropdown {
+public class Practice {
 
 	public static void main(String[] args) throws InterruptedException {
-
-        // Launch the browser window
+		
+		// Launch the browser window
 	    WebDriver driver = new ChromeDriver();
 	    
 	    // Implicit Wait
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	    
 	    // Open URL in the browser window
-	    driver.get("https://www.google.com/");
+	    driver.get("https://www.swiggy.com/search");
 	    
 	    // Maximize the browser window
 	    driver.manage().window().maximize();
 	    
-	    // Google search textarea 
-	    driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys("selenium");
+	    // Auto Suggestion dropdown
+	    driver.findElement(By.xpath("//input[@class='ssM7E']")).sendKeys("Maggi");
 	    
 	    Thread.sleep(5000);
 	    
-	    List<WebElement> list = driver.findElements(By.xpath("//ul[@role='listbox']//li//div[@role='option']"));
+	    List<WebElement> list = driver.findElements(By.xpath("//button[contains(@class,'xN32R')]"));
 	    
-	    System.out.println("Size of the list :"+list.size());
+        System.out.println("Size of the list :"+list.size());
 	    
 	    for(int i = 0 ; i < list.size(); i++) {
 	    	
 	    	System.out.println(list.get(i).getText());
-	    	
-	    	if(list.get(i).getText().equals("selenium")) {
-	    		list.get(i).click();
-	    		break;
-	    	}
 	    }
 	    
-	    
-	    
+
 	}
 
 }
