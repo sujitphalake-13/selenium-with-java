e8bb14c990cc046a908ad759efe4488f66a9ede2
sujitphalake-13 <sujit.phalake13@gmail.com>
2025-10-31 19:32:26 +0530

    Drag and Drop Mouse action

--- src/test/java/day35/MouseActions.java
+++ src/test/java/day35/MouseActions.java
@@ -1,8 +1,13 @@
 package day35;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class MouseActions {
@@ -20,7 +25,38 @@ public class MouseActions {
 		
 		// Maximize the size of browser window
 		driver.manage().window().maximize();
+		
+		// Actions class for Mouse actions
+		Actions action = new Actions(driver);
+		
+		// 1. Mouse Hover action
+		/*
+		WebElement hover = driver.findElement(By.xpath("//button[normalize-space()='Point Me']"));
+		
+		Actions act = action.moveToElement(hover);
 
+		act.build();             // create an action
+		act.perform();           // build an action
+		*/
+		
+		// 2. Mouse Double click action
+        
+		Actions dAct = action.doubleClick(driver.findElement(By.xpath("//button[normalize-space()='Copy Text']")));
+		dAct.build();           // create an action
+		dAct.perform();         // build an action
+		
+		
+		// 3. Mouse Right Click action
+		
+		//action.contextClick(driver.findElement(By.xpath("//button[@class='start']"))).build().perform();
+		
+		
+		// 4. Mouse Drag and Drop action
+		
+		//driver.findElement(By.xpath(""));
+		
+		
+		
 	}
 
 }
