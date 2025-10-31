68771f3ed6cf805b7cc88032a9491c9a24c454c7
sujitphalake-13 <sujit.phalake13@gmail.com>
2025-10-31 11:23:01 +0530

    Implement Implicit wait in bootstrap dropdown

--- src/test/java/day31/BootstrapDropdown.java
+++ src/test/java/day31/BootstrapDropdown.java
@@ -4,27 +4,16 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 
 public class BootstrapDropdown {
 	
-
 	public static void main(String[] args) {
 		
 		// Launch the browser window
 		WebDriver driver = new ChromeDriver();
 		
-		// FluentWait setup
-		Wait<WebDriver> myWait = new FluentWait<>(driver)
-		    .withTimeout(Duration.ofSeconds(20))
-		    .pollingEvery(Duration.ofSeconds(5))
-		    .ignoring(Exception.class);
-
-		// Wait for the cookie button and click
-		myWait.until(ExpectedConditions.elementToBeClickable(By.id("accept_cookies_btn"))).click();
-				
+		// Implicit Wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));		
 		// Open URL in the browser window
 		driver.get("https://mdbootstrap.com/docs/standard/extended/multiselect/");
 		
